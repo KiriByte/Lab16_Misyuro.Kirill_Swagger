68df85f7f80f28e4f578773e8f6d02a7c82c5c2b
KiriByte <kmisyuro@gmial.com>
2023-03-17 15:18:47 +0300

    delete unused method

--- Lab16_Misyuro.Kirill_Swagger/Service/FilmsService.cs
+++ Lab16_Misyuro.Kirill_Swagger/Service/FilmsService.cs
@@ -14,28 +14,6 @@ public class FilmsService
         Films = JsonConvert.DeserializeObject<Top250Data>(json).Items;
     }
 
-    public bool UpdateFilm(string id, FilmDetail film)
-    {
-        var foundFilm = Films.Find(p => p?.Rank == id);
-        if (foundFilm != null)
-        {
-            foundFilm.Id = film.Id;
-            foundFilm.Rank = film.Rank;
-            foundFilm.Crew = film.Crew;
-            foundFilm.Year = film.Year;
-            foundFilm.Image = film.Image;
-            foundFilm.Title = film.Title;
-            foundFilm.FullTitle = film.FullTitle;
-            foundFilm.IMDbRating = film.IMDbRating;
-            foundFilm.IMDbRatingCount = film.IMDbRatingCount;
-            
-            Serialize();
-            return true;
-        }
-
-        return false;
-    }
-
     private string ReadFileToString()
     {
         if (!File.Exists(_pathToFile))
