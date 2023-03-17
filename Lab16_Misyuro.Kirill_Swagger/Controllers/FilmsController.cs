db2254d89626357a2923ab0a395fd55d096dc919
KiriByte <kmisyuro@gmial.com>
2023-03-17 23:16:13 +0300

    Added documentation

--- Lab16_Misyuro.Kirill_Swagger/Controllers/FilmsController.cs
+++ Lab16_Misyuro.Kirill_Swagger/Controllers/FilmsController.cs
@@ -15,6 +15,10 @@ public class FilmsController : Controller
         _service = service;
     }
 
+    /// <summary>
+    /// Get all films from list
+    /// </summary>
+    /// <remarks>This method doesnt take any parameters, because it print all list of films</remarks>
     [HttpGet]
     [Route("GetAllFilms")]
     public List<FilmDetail?> GetAllFilms()
@@ -22,6 +26,12 @@ public class FilmsController : Controller
         return _service.Films;
     }
 
+    /// <summary>
+    /// Get film from list by id
+    /// </summary>
+    /// <remarks>This method search film from list by id and print it.If film not found, return error 404.</remarks>
+    /// <response code="200">Film found</response>
+    /// <response code="404">Film not found</response>
     [HttpGet]
     [Route("GetById/{id}")]
     public IActionResult GetById(string id)
@@ -35,6 +45,12 @@ public class FilmsController : Controller
         return NotFound();
     }
 
+    /// <summary>
+    /// Get film from list by rank
+    /// </summary>
+    /// <remarks>This method search film from list by rank and print it.If film not found, return error 404.</remarks>
+    /// <response code="200">Film found</response>
+    /// <response code="404">Film not found</response>
     [HttpGet]
     [Route("GetByRank/{rank}")]
     public IActionResult GetByRank(string rank)
@@ -48,6 +64,10 @@ public class FilmsController : Controller
         return NotFound();
     }
 
+    /// <summary>
+    /// Add film to list
+    /// </summary>
+    /// <remarks>This method add film to the list. At the moment there is no check for the uniqueness of the id.</remarks>
     [HttpPost]
     [Route("AddFilm")]
     public IActionResult AddFilm(FilmDetail film)
@@ -57,6 +77,12 @@ public class FilmsController : Controller
         return Ok(film);
     }
 
+    /// <summary>
+    /// Update film from list by id
+    /// </summary>
+    /// <remarks>This method update film from list by id.If film not found, return error 404.</remarks>
+    /// <response code="200">Film updated</response>
+    /// <response code="404">Film not found</response>
     [HttpPut]
     [Route("UpdateFilm/{id}")]
     public IActionResult UpdateFilm(string id, FilmDetail film)
@@ -81,6 +107,12 @@ public class FilmsController : Controller
         return NotFound();
     }
 
+    /// <summary>
+    /// Delete film from list by id
+    /// </summary>
+    /// <remarks>This method delete film from list by id.If film not found, return error 404.</remarks>
+    /// <response code="200">Film deleted</response>
+    /// <response code="404">Film not found</response>
     [HttpDelete]
     [Route("DeleteFilm/{id}")]
     public IActionResult DeleteFilm(string id)
--- Lab16_Misyuro.Kirill_Swagger/Program.cs
+++ Lab16_Misyuro.Kirill_Swagger/Program.cs
@@ -1,4 +1,6 @@
 using Lab16_Misyuro.Kirill_Swagger.Service;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var basePath = AppContext.BaseDirectory;
+    var xmlPath = Path.Combine(basePath, "Lab16_Misyuro.Kirill_Swagger.xml");
+    options.IncludeXmlComments(xmlPath);
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Top 250 Films API",
+        Description = @"Project GetTop250IMDB takes a list of top 250 movies by IMDB rating.
+This API uses this list and performs CRUD operations.",
+        Contact = new OpenApiContact
+        {
+            Name = "Kirill Misyuro",
+            Url = new Uri("https://github.com/KiriByte")
+        },
+    });
+});
+
 
 builder.Services.AddTransient<FilmsService>();
 
